d9099dc690287287401bfc6b12f7cfaf54b14cde
renpengfei@hotmail.com <renpengfei@hotmail.com>
2016-04-28 15:11:49 +0800

    ceshi

---
+++ src/com/test/Hell2.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author "Ren PengFei (ROC)"
+ * @date 2016年4月28日 下午2:41:05
+ * @description
+ */
+public class Hell2 {
+
+	/**
+	 * @author "Ren PengFei (ROC)"
+	 * @date 2016年4月28日 下午2:41:05
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author "Ren PengFei (ROC)"
+ * @date 2016年4月28日 下午2:23:20
+ * @description
+ */
+public class HelloWorld {
+
+	/**
+	 * @author "Ren PengFei (ROC)"
+	 * @date 2016年4月28日 下午2:23:20
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello ");
+
+	}
+
+}
